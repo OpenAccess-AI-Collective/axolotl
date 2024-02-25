acbd19315bc8f2641ed8ed7ad80b9c0797c7005b
Monk <aadityaura@gmail.com>
2024-02-25 17:46:07 +0530

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -14,16 +14,18 @@ else
     echo "Installing Miniconda..."
     bash Miniconda3-latest-Linux-x86_64.sh -b
 
-    # Export Miniconda path temporarily for the script
+    # Immediately export Miniconda path for the current script execution
     export PATH="$HOME/miniconda3/bin:$PATH"
+
+    # Append the export command to ~/.bashrc for future sessions
+    echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
 
     # Initialize Conda for bash shell
     echo "Initializing Conda..."
     conda init bash
-
-    # Refresh shell to apply conda init changes. Using source to avoid starting a new shell.
-    source ~/.bashrc
 fi
+
+# No need to source ~/.bashrc here since we've already manually updated PATH
 
 # Create a new Conda environment with Python 3.10
 echo "Creating a new Conda environment 'axolotl' with Python 3.10..."
@@ -34,26 +36,19 @@ conda init bash
 
 # Activate the newly created Conda environment
 echo "Activating the 'axolotl' environment..."
-source activate axolotl
+source activate axolotl || source $HOME/miniconda3/bin/activate axolotl
 
 # Install CUDA and PyTorch with dependencies
 echo "Installing CUDA and PyTorch with dependencies..."
 conda install -y -c "nvidia/label/cuda-12.1.1" cuda --yes
 conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia --yes
 
-# Assuming the script is run from the root of the cloned 'axolotl' repository, so no need to clone it again
 # Install additional Python packages
 echo "Installing additional Python packages..."
 pip3 install packaging
 pip3 install -e '.[flash-attn,deepspeed]'
 
-# Append the environment activation command to the user's shell startup file
-if [[ -f ~/.bashrc ]]; then
-    echo "conda activate axolotl" >> ~/.bashrc
-    echo "Conda 'axolotl' environment will be automatically activated in new bash sessions."
-elif [[ -f ~/.zshrc ]]; then
-    echo "conda activate axolotl" >> ~/.zshrc
-    echo "Conda 'axolotl' environment will be automatically activated in new zsh sessions."
-fi
+pip3 uninstall deepspeed -y
+pip3 install deepspeed==0.13.1 -y
 
-echo "Setup completed successfully."
+echo "Setup completed successfully.
